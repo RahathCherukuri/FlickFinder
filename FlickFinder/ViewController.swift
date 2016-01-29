bcfa438d0c64f95bcb4b5ae3d9703c4d1d5286b5
Rahath Cherukuri <rahath.cherukuri@gmail.com>
2016-01-28 21:21:47 -0500

    Added code for Randomization of photos using max pages.

--- FlickFinder/ViewController.swift
+++ FlickFinder/ViewController.swift
@@ -75,7 +75,7 @@ class ViewController: UIViewController {
         var searchText = searchStringTextField.text!
         searchText = searchText.stringByReplacingOccurrencesOfString(" ", withString: "+", options: .LiteralSearch, range: nil)
         methodArguments["text"] = searchText
-        print("methodArguments: ",methodArguments)
+//        print("methodArguments: ",methodArguments)
         getImageFromFlickrBySearch(methodArguments)
     }
     
@@ -90,7 +90,7 @@ class ViewController: UIViewController {
             "nojsoncallback": NO_JSON_CALLBACK
         ]
         methodArguments["bbox"] = createBoundingBoxString()
-        print("methodArguments: ",methodArguments)
+//        print("methodArguments: ",methodArguments)
         getImageFromFlickrBySearch(methodArguments)
     }
     
@@ -151,44 +151,169 @@ class ViewController: UIViewController {
                 return
             }
             
-            guard let photoCount = photosDictionary["photo"]?.count else {
+            guard let totalPages = photosDictionary["pages"] as? Int else {
                 print("There are no photos")
                 return
             }
-            print(photoCount)
-            if photoCount > 0 {
-                let arrayIndex = self.randomValue(photoCount)
-                print("\narrayIndex: ", arrayIndex)
+            
+            /* Flickr API will only return up the 4000 images (100 per page * 40 page max) */
+            /* Pick a random page! */
+            let pageLimit = min(totalPages, 40)
+            let randomPage = Int(arc4random_uniform(UInt32(pageLimit))) + 1
+            self.getImageFromFlickrBySearchWithPage(methodArguments, pageNumber: randomPage)
+            
+            
+//            if photoCount > 0 {
+//                let arrayIndex = self.randomValue(photoCount)
+//                print("\narrayIndex: ", arrayIndex)
+//                
+//                guard let photos = photosDictionary["photo"] else {
+//                    print("There are no photos")
+//                    return
+//                }
+//                print("RandomPhoto: ", photos[arrayIndex])
+//                
+//                guard let imageUrlString = photos[arrayIndex]["url_m"] as? String,
+//                    let photoTitle = photos[arrayIndex]["title"] as? String else {
+//                        print("There is no url for the image")
+//                        return
+//                }
+//                print("URL: ", imageUrlString)
+//                
+//                let imageURL = NSURL(string: imageUrlString)
+//                
+//                /* 6 - Update the UI on the main thread */
+//                if let imageData = NSData(contentsOfURL: imageURL!) {
+//                    dispatch_async(dispatch_get_main_queue(), {
+//                        self.imageTitle.text = photoTitle
+//                        self.photoImageView.image = UIImage(data: imageData)
+//                    })
+//                }
+//            } else {
+//                dispatch_async(dispatch_get_main_queue(), {
+//                    print("No Photos Found. Search Again.")
+//                    self.imageTitle.text = "No Photos Found. Search Again."
+//                })
+//            }
+        }
+        task.resume()
+    }
+    
+    
+    func getImageFromFlickrBySearchWithPage(methodArguments: [String : AnyObject], pageNumber: Int) {
+        
+        /* Add the page to the method's arguments */
+        var withPageDictionary = methodArguments
+        withPageDictionary["page"] = pageNumber
+        
+        let session = NSURLSession.sharedSession()
+        let urlString = BASE_URL + escapedParameters(withPageDictionary)
+        let url = NSURL(string: urlString)!
+        let request = NSURLRequest(URL: url)
+        
+        let task = session.dataTaskWithRequest(request) { (data, response, error) in
+            
+            /* GUARD: Was there an error? */
+            guard (error == nil) else {
+                print("There was an error with your request: \(error)")
+                return
+            }
+            
+            /* GUARD: Did we get a successful 2XX response? */
+            guard let statusCode = (response as? NSHTTPURLResponse)?.statusCode where statusCode >= 200 && statusCode <= 299 else {
+                if let response = response as? NSHTTPURLResponse {
+                    print("Your request returned an invalid response! Status code: \(response.statusCode)!")
+                } else if let response = response {
+                    print("Your request returned an invalid response! Response: \(response)!")
+                } else {
+                    print("Your request returned an invalid response!")
+                }
+                return
+            }
+            
+            /* GUARD: Was there any data returned? */
+            guard let data = data else {
+                print("No data was returned by the request!")
+                return
+            }
+            
+            /* Parse the data! */
+            let parsedResult: AnyObject!
+            do {
+                parsedResult = try NSJSONSerialization.JSONObjectWithData(data, options: .AllowFragments)
+            } catch {
+                parsedResult = nil
+                print("Could not parse the data as JSON: '\(data)'")
+                return
+            }
+            
+            /* GUARD: Did Flickr return an error (stat != ok)? */
+            guard let stat = parsedResult["stat"] as? String where stat == "ok" else {
+                print("Flickr API returned an error. See error code and message in \(parsedResult)")
+                return
+            }
+            
+            /* GUARD: Is the "photos" key in our result? */
+            guard let photosDictionary = parsedResult["photos"] as? NSDictionary else {
+                print("Cannot find key 'photos' in \(parsedResult)")
+                return
+            }
+            
+            /* GUARD: Is the "total" key in photosDictionary? */
+            guard let totalPhotosVal = (photosDictionary["total"] as? NSString)?.integerValue else {
+                print("Cannot find key 'total' in \(photosDictionary)")
+                return
+            }
+            
+            print("photosDictionary: ", photosDictionary)
+            
+            if totalPhotosVal > 0 {
                 
-                guard let photos = photosDictionary["photo"] else {
-                    print("There are no photos")
+                /* GUARD: Is the "photo" key in photosDictionary? */
+                guard let photosArray = photosDictionary["photo"] as? [[String: AnyObject]] else {
+                    print("Cannot find key 'photo' in \(photosDictionary)")
                     return
                 }
-                print("RandomPhoto: ", photos[arrayIndex])
                 
-                guard let imageUrlString = photos[arrayIndex]["url_m"] as? String,
-                    let photoTitle = photos[arrayIndex]["title"] as? String else {
-                        print("There is no url for the image")
-                        return
+                let randomPhotoIndex = Int(arc4random_uniform(UInt32(photosArray.count)))
+                let photoDictionary = photosArray[randomPhotoIndex] as [String: AnyObject]
+                let photoTitle = photoDictionary["title"] as? String /* non-fatal */
+                
+                /* GUARD: Does our photo have a key for 'url_m'? */
+                guard let imageUrlString = photoDictionary["url_m"] as? String else {
+                    print("Cannot find key 'url_m' in \(photoDictionary)")
+                    return
                 }
-                print("URL: ", imageUrlString)
                 
                 let imageURL = NSURL(string: imageUrlString)
+                print("imageURL: ", imageURL)
                 
-                /* 6 - Update the UI on the main thread */
                 if let imageData = NSData(contentsOfURL: imageURL!) {
                     dispatch_async(dispatch_get_main_queue(), {
-                        self.imageTitle.text = photoTitle
+                        
                         self.photoImageView.image = UIImage(data: imageData)
+                        
+                        if methodArguments["bbox"] != nil {
+                            if let photoTitle = photoTitle {
+                                self.imageTitle.text = "\(photoTitle)"
+                            } else {
+                                self.imageTitle.text = " \("Untitled")"
+                            }
+                        } else {
+                            self.imageTitle.text = photoTitle ?? "(Untitled)"
+                        }
                     })
+                } else {
+                    print("Image does not exist at \(imageURL)")
                 }
             } else {
                 dispatch_async(dispatch_get_main_queue(), {
-                    print("No Photos Found. Search Again.")
                     self.imageTitle.text = "No Photos Found. Search Again."
+                    self.photoImageView.image = nil
                 })
             }
         }
+        
         task.resume()
     }
     
